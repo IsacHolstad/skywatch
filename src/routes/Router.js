6c862f2064226f2db7e19e22a2d9a5d1fc514d82
IsacHolstad <wisac22@gmail.com>
2023-06-27 11:09:15 +0200

    added tailwind and router

---
+++ src/routes/Router.js
@@ -0,0 +1,14 @@
+import {Route, Routes} from "react-router-dom";
+import Home from "../views/Home";
+
+function Router() {
+    return(
+        <>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+            </Routes>
+
+        </>
+    )
+}
+export default Router;
