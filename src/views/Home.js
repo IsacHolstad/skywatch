411085cc749251a0bd7483faea15c7a73c959fce
IsacHolstad <wisac22@gmail.com>
2023-06-29 00:16:12 +0200

    logging uvdata

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,16 +1,20 @@
 import {useState} from "react";
+import {json} from "react-router-dom";
 function Home() {
     const [data, setData] = useState('');
 
     const fetchWeather = () => {
         try{
             let response = fetch( 'https://api.openuv.io/api/v1/forecast?lat=:lat&lng=:lng&alt=:alt&dt=:dt')
+            const uvData = response.json()
+            console.log(uvData)
         }
         catch (error) {
             console.log(error)
         }
 
     }
+    fetchWeather()
 
 
 
