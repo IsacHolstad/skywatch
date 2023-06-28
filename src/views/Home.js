be0cdc5437045434b666515361dae4171d280175
IsacHolstad <wisac22@gmail.com>
2023-06-29 00:10:38 +0200

    doing get request to get uv grade for siggerud

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,23 @@
+import {useState} from "react";
 function Home() {
-    return null
+    const [data, setData] = useState('');
+
+    const fetchWeather = () => {
+        try{
+            let response = fetch( 'https://api.openuv.io/api/v1/forecast?lat=:lat&lng=:lng&alt=:alt&dt=:dt')
+        }
+        catch (error) {
+            console.log(error)
+        }
+
+    }
+
+
+
+    return (
+        <>
+        </>
+    )
 }
 
 export default Home;
