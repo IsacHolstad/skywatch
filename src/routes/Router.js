4df826df386848838c14945d4ca4daa17cefd632
IsacHolstad <wisac22@gmail.com>
2023-06-28 23:50:23 +0200

    made location page

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,11 +1,13 @@
 import {Route, Routes} from "react-router-dom";
 import Home from "../views/Home";
+import Location from "../views/Location";
 
 function Router() {
     return(
         <>
             <Routes>
                 <Route path="/" element={<Home/>}/>
+                <Route path="/" element={<Location/>}/>
             </Routes>
 
         </>
