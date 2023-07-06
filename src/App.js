53c0425d342cb33d73633a87f0e25f21d5533dd7
IsacHolstad <wisac22@gmail.com>
2023-07-06 12:29:09 +0200

    footer imported

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './index.css';
 import Navigation from "./components/Navigation";
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navigation/>
-
-    </div>
+      <Footer/>
+    </>
   );
 }
 
