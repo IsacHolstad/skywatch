29fdf238d8347b859b4b400f7bf305e23e55cf6e
IsacHolstad <wisac22@gmail.com>
2023-07-14 00:02:08 +0200

    footer info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className="w-full h-12 bg-red-300">
+            footer
         </div>
     );
 };
