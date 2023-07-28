e3f396d756d617fac9d131dee472f3e53117aa00
IsacHolstad <wisac22@gmail.com>
2023-07-28 11:52:06 +0200

    height and width fix

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,7 +8,7 @@ const Search = () => {
                        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                 <div className="relative">
                     <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                        <svg className="w-6 h-6 text-gray-900 dark:text-gray-400" aria-hidden="true"
+                        <svg className="w-4 h-4 text-gray-900 dark:text-gray-400" aria-hidden="true"
                              xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                   d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
