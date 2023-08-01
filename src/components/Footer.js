ef04cbb17b69ec2d160c5062508e76232334c0e7
IsacHolstad <wisac22@gmail.com>
2023-08-01 14:12:22 +0200

    footer updated text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="w-full h-12 bg-black border-gray-500 border-1">
-            footer
+        <div className="w-full h-14 bg-black border-gray-500 border-1 text-gray-300 text-sm text-center py-2">
+            All rights reserved to @Skywatch
         </div>
     );
 };
