504f73be0294531b063c36576d38b6d7ce1ace5b
IsacHolstad <wisac22@gmail.com>
2023-08-01 14:09:58 +0200

    creators section made

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './index.css';
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer"
+import CreatorsSection from "./components/CreatorsSection";
 
 function App() {
   return (
     <>
       <Navigation/>
+        <CreatorsSection/>
       <Footer/>
     </>
   );
