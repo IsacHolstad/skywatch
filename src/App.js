918822a99b8318c10eebab4e371d06cde5e1ba31
IsacHolstad <wisac22@gmail.com>
2023-08-02 12:46:22 +0200

    weather section data update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './index.css';
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer"
 import CreatorsSection from "./components/CreatorsSection";
+import WeatherSection from "./components/WeatherSection";
 
 function App() {
   return (
     <>
       <Navigation/>
         <CreatorsSection/>
-        <
+        <WeatherSection/>
       <Footer/>
     </>
   );
